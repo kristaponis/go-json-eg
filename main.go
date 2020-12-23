d656c64065a17106198f802cdff6b9f36cba8f28
kristaponis <a.kristaponis@icloud.com>
2020-12-23 15:40:57 +0200

    Added demo versions of marshal unmarshal

--- main.go
+++ main.go
@@ -1,5 +1,40 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+)
+
 func main() {
-	
+	demo2()
+}
+
+func demo() {
+	prdct := product{
+		ID:       "1",
+		Name:     "product1",
+		Price:    3,
+		Quantity: 10,
+		Status:   true,
+	}
+	res, err := json.Marshal(prdct)
+	if err != nil {
+		log.Println(err)
+	}
+	fmt.Println(string(res))
+}
+
+func demo2() {
+	var prdct product
+	str := `{
+		"id":"1",
+		"name":"product1",
+		"price":3,
+		"quantity":10,
+		"status":true
+	}`
+	json.Unmarshal([]byte(str), &prdct)
+	fmt.Println(prdct)
+	fmt.Println(prdct.toStr())
 }
--- product.go
+++ product.go
@@ -1,11 +1,15 @@
 package main
 
+import "fmt"
+
 type product struct {
-	ID       string   `json:"id"`
-	Name     string   `json:"name"`
-	Price    float32  `json:"price"`
-	Quantity int      `json:"quantity"`
-	Status   bool     `json:"status"`
-	Category category `json:"category"`
+	ID       string  `json:"id"`
+	Name     string  `json:"name"`
+	Price    float32 `json:"price"`
+	Quantity int     `json:"quantity"`
+	Status   bool    `json:"status"`
 }
-		
+
+func (p product) toStr() string {
+	return fmt.Sprintf("Id: %s\nName: %s\nPrice: %f\nQuantity: %d\nStatus: %t", p.ID, p.Name, p.Price, p.Quantity, p.Status)
+}
